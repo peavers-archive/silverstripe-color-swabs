e007f6f4e7db0783d5a27664a6b1c8154e9b780b
Chris Turner <chris.turner@minedu.govt.nz>
2015-07-28 15:57:10 +1200

    Typo in docs

--- js/color-swabs.js
+++ js/color-swabs.js
@@ -1,7 +1,7 @@
 /**
  * This is the set of classes that will be created for each item in the dropdown. Either replace this with your own
  * array of items, or just modify as needed. These elements should match classes in the color-swabs.css file. This
- * is the only file javascript that you need to modify.
+ * is the only javascript file that you need to modify.
  *
  * @type {string[]}
  */
